d2cdd166ab6a51dfb05fb0a7fbaee4873f9c3cb9
RakibulRanak <rakibulhasanranak1@gmail.com>
2021-10-01 18:13:00 +0600

    basic node server implemented

---
+++ app.js
@@ -0,0 +1,24 @@
+'use strict';
+// Imports
+const express = require('express');
+const morgan = require('morgan');
+
+// Creating the express app
+const app = express();
+
+// Parsing JSON and Cookies
+app.use(express.json({ limit: '1000kb' }));
+
+
+app.use(morgan('dev'));
+
+app.get('/api/v1', (req, res) => {
+  res.send('Hello');
+});
+
+app.all('*', (req, res, next) => {
+    res.send(`Can't find ${req.originalUrl} on this server!`);
+});
+
+// Exporting the app
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+require('dotenv').config();
+
+const PORT = process.env.PORT || 8000;
+const HOST = process.env.HOST || 'localhost';
+
+// Importing the express app
+const app = require('./app');
+
+// Starting the server
+const server = app.listen(PORT, HOST, () => {
+  console.log(`Server started on ${HOST}:${PORT}`);
+});
+
+// Handle Unhandled Rejections
+process.on('unhandledRejection', (err) => {
+  console.log('Unhandled Rejection! Shutting down the server...');
+  console.error(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
