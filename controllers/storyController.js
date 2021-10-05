fe799e259d6992c138c7bcce661e699b8f310d66
RakibulRanak <rakibulhasanranak1@gmail.com>
2021-10-05 18:26:41 +0600

    negotiate content in storycontroller

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -1,55 +1,79 @@
 const catchAsync = require('../errors/catchAsync');
 const Story = require('../models/storyModel');
 const AppError = require('../errors/appError');
+const js2xmlparser = require("js2xmlparser");
+
+const sendJsonResponse = (req, res, statusCode, status, message, data) => {
+    return res.status(statusCode).json({
+        status,
+        message,
+        data,
+    });
+};
+const sendXmlResponse = (req, res, statusCode, data) => {
+    return res.status(statusCode).send(js2xmlparser.parse("data", data));
+};
+
+const sendDefaultResponse = (req, res, statusCode) => {
+    return res.status(statusCode).send(`<html>
+                <body>
+                    accept content-type doesn't match any    
+                </body>
+        </html>`
+    );
+};
 
 exports.createStory = catchAsync(async (req, res, next) => {
     const story = await Story.create(req.body);
-    res.status(201).json({
-        status: 'success',
-        message: 'Story is created successfully',
-        story
+    res.format({
+        'application/json': () => sendJsonResponse(req, res, 201, 'success', 'Story created successfully', story),
+        'application/xml': () => sendXmlResponse(req, res, 201, story),
+        'default': () => sendDefaultResponse(req, res, 201)
     });
 });
 
-
-
 exports.getStory = catchAsync(async (req, res, next) => {
-    const story = await Story.findOne({ where: { id: req.params.id } });
+    const story = await Story.findOne({
+        raw: true,
+        where: { id: req.params.id }, attributes: ['id', 'title', 'story', ['username', 'author'], 'createdAt']
+    });
     if (story == null) {
         return next(new AppError(`Not found`, 404));
     }
-    res.status(200).json({
-        status: 'success',
-        message: 'Story fetched successfully',
-        data: story
+    res.format({
+        'application/json': () => sendJsonResponse(req, res, 200, 'success', 'Story fetched successfully', story),
+        'application/xml': () => sendXmlResponse(req, res, 200, story),
+        'default': () => sendDefaultResponse(req, res, 200)
     });
+
 });
 
 exports.getStories = catchAsync(async (req, res, next) => {
-    const stories = await Story.findAll();
-    res.status(200).json({
-        status: 'success',
-        message: 'Stories fetched successfully',
-        data: stories
+    const stories = await Story.findAll({ raw: true, attributes: ['id', 'title', 'story', ['username', 'author'], 'createdAt'] });
+    res.format({
+        'application/json': () => sendJsonResponse(req, res, 200, 'success', 'Stories fetched successfully', stories),
+        'application/xml': () => sendXmlResponse(req, res, 200, stories),
+        'default': () => sendDefaultResponse(req, res, 200)
     });
 });
 
 exports.updateStory = catchAsync(async (req, res, next) => {
     story = await Story.update(req.body, { returning: true, where: { id: req.params.id } });
-    res.status(200).json({
-        status: 'success',
-        message: 'Story updated successfully',
-        data: story[1][0]
+    res.format({
+        'application/json': () => sendJsonResponse(req, res, 200, 'success', 'Story updated successfully', story[1][0]),
+        'application/xml': () => sendXmlResponse(req, res, 200, story[1][0]),
+        'default': () => sendDefaultResponse(req, res, 200)
     });
 });
 
 exports.deleteStory = catchAsync(async (req, res, next) => {
     await Story.destroy({ where: { id: req.params.id } }).then((rowDeleted) => {
         if (rowDeleted) {
-            res.status(200).json({
-                status: 'success',
-                message: "Successfully deleted"
-            })
+            res.format({
+                'application/json': () => sendJsonResponse(req, res, 200, 'success', 'Story deleted successfully', ''),
+                'application/xml': () => sendXmlResponse(req, res, 200, 'story deleted successfully'),
+                'default': () => sendDefaultResponse(req, res, 200)
+            });
         }
         else {
             return next(new AppError(`Story not found`, 404));
