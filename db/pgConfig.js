42516646d2842625737ad9463ccf9aa095917e21
RakibulRanak <rakibulhasanranak1@gmail.com>
2023-03-03 12:30:44 +0600

    netlify

--- db/pgConfig.js
+++ db/pgConfig.js
@@ -1,7 +1,6 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 const DB_URI = (process.env.NODE_ENV == 'test') ? process.env.DB_URI_TEST : process.env.DB_URI;
-console.log(DB_URI)
 const sequelize = new Sequelize(
   DB_URI,
   {
